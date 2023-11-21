73fad4c7552accffa8b8e406ddfc27f2b9f3967e
nameer-rizvi <rizvinameer@gmail.com>
2023-11-20 20:52:21 -0500

    fix rsi for small numbers

--- src/squezapi/wsj.js
+++ src/squezapi/wsj.js
@@ -230,7 +230,7 @@ function assignRSIData(candle, series, OPTION) {
     for (let i = 0; i < series.length; i++) {
       let currPrice = series[i]?.priceLast;
       let prevPrice = series[i - 1]?.priceLast;
-      let change = simpul.math.change.num(prevPrice, currPrice);
+      let change = prevPrice - currPrice;
       if (change > 0) gain += change;
       if (change < 0) loss += Math.abs(change);
     }
@@ -243,7 +243,7 @@ function assignRSIData(candle, series, OPTION) {
   } else if (prev?.rsi) {
     let gain = prev.averageGain * 13;
     let loss = prev.averageLoss * 13;
-    let change = simpul.math.change.num(prev.priceLast, candle.priceLast);
+    let change = prev.priceLast - candle.priceLast;
     if (change > 0) gain += change;
     if (change < 0) loss += Math.abs(change);
     let averageGain = gain / 14;
