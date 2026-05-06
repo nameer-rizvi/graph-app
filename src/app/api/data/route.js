468a13ff61d3d372e9f56c408a1200dd6a895d3d
nameer-rizvi <rizvinameer@gmail.com>
2026-05-06 14:45:39 -0400

    Remove console.log

--- src/app/api/data/route.js
+++ src/app/api/data/route.js
@@ -13,7 +13,6 @@ export async function GET(request) {
 
     return NextResponse.json(data);
   } catch (error) {
-    console.log(error);
     return NextResponse.json(error.message, { status: 400 });
   }
 }
